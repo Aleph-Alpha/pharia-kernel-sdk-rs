9c8a145cd063d452107b2a9add8150703d1d5f1f
Ben Brandt <benjamin.j.brandt@gmail.com>
2024-10-12 12:04:55 +0200

    docs: update example to use llama prompt format

--- examples/hello-world/src/lib.rs
+++ examples/hello-world/src/lib.rs
@@ -1,18 +1,17 @@
-use pharia_skill::{skill, CompletionParams, Csi};
+use pharia_skill::{prompt::llama3_instruct::Prompt, skill, CompletionParams, Csi};
 
 // This can also return an `anyhow::Result<String>` if you need handle errors.
 #[skill]
 fn hello_world(csi: &impl Csi, name: &str) -> String {
-    let prompt = format!(
-        "<|begin_of_text|><|start_header_id|>system<|end_header_id|>
-
-Cutting Knowledge Date: December 2023
+    let prompt = Prompt::new(
+        "Cutting Knowledge Date: December 2023
 Today Date: 23 Jul 2024
 
-You are a helpful assistant.<|eot_id|><|start_header_id|>user<|end_header_id|>
-
-Provide a nice greeting for the person named: {name}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"
-    );
+You are a helpful assistant.",
+    )
+    .with_user_message(format!(
+        "Provide a nice greeting for the person named: {name}"
+    ));
 
     let result = csi.complete(
         "llama-3.1-8b-instruct",
