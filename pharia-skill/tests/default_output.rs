beec1e62efec52a0ce973b5a89cbbf2d808c704d
Ben Brandt <benjamin.j.brandt@gmail.com>
2024-10-12 14:14:57 +0200

    feat: add complete_all method to Csi

--- pharia-skill/tests/default_output.rs
+++ pharia-skill/tests/default_output.rs
@@ -2,17 +2,15 @@ use pharia_skill::{Completion, CompletionParams, CompletionRequest, Csi, FinishR
 
 #[pharia_skill::skill]
 fn can_compile(csi: &impl Csi, input: Vec<&str>) -> Vec<String> {
-    input
-        .into_iter()
-        .map(|input| {
-            csi.complete(&CompletionRequest::new(
-                "hello",
-                input,
-                CompletionParams::default(),
-            ))
-            .text
-        })
-        .collect()
+    csi.complete_all(
+        &input
+            .into_iter()
+            .map(|i| CompletionRequest::new("hello", i, CompletionParams::default()))
+            .collect::<Vec<_>>(),
+    )
+    .into_iter()
+    .map(|c| c.text)
+    .collect()
 }
 
 struct MockCsi;
@@ -20,7 +18,7 @@ struct MockCsi;
 impl pharia_skill::Csi for MockCsi {
     fn complete(&self, request: &CompletionRequest<'_>) -> Completion {
         Completion {
-            text: request.prompt.clone(),
+            text: request.prompt.clone().into_owned(),
             finish_reason: FinishReason::Stop,
         }
     }
